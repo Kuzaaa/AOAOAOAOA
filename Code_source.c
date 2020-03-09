f0905aad6ad7e9d91ba757c8aeb537e6635edb2e
rkettemeyer <r.kettemeyer@gmail.com>
2020-03-09 15:15:18 +0100

    Init done

--- Code_source.c
+++ Code_source.c
@@ -1,13 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 32
 
-void baseline(unsigned n, float a[n][n], float b[n][n], float c[n][n]);
 
-int main(){
+void baseline(unsigned n, float** a, float** b, float** c);
 
-  baseline();
+int main(int argc, char *argv[]){
+
+  if(argc<2||argc>2){
+    return 0;
+  }
+
+  float** a = NULL;
+  float** b = NULL;
+  float** c = NULL;
+
+  unsigned i,j,n;
+  n = (int)atoi(argv[1]);
+  srand(time(NULL));
+
+  a = malloc(n*sizeof(float*));
+  b = malloc(n*sizeof(float*));
+  c = malloc(n*sizeof(float*));
+  for(i=0; i<n; i++){
+    a[i] = malloc(n*sizeof(float));
+    b[i] = malloc(n*sizeof(float));
+    c[i] = malloc(n*sizeof(float));
+    for(j=0; j<n; j++){
+      a[i][j] = ((float)rand())/MAX;
+      b[i][j] = ((float)rand())/MAX;
+      c[i][j] = ((float)rand())/MAX;
+    }
+  }
+
+  baseline(n,a,b,c);
+
+ //Il faudrait free mais j'ai la flemme
 }
 
-void baseline(unsigned n, float a[n][n], float b[n][n], float c[n][n]){
+void baseline(unsigned n, float** a, float** b, float** c){
   unsigned i, j;
 
   for(i=0; i<n; i++){
