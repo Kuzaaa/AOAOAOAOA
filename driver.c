99b23e0e0069e2292d1a917884b932431b5d9118
Mushroom742 <solenn.thomas@hotmail.fr>
2020-03-19 11:39:49 +0100

    suppression du calcul du temps

--- driver.c
+++ driver.c
@@ -3,7 +3,7 @@
 #include <time.h>
 #include <stdint.h>
 
-#define NB_METAS 1
+#define NB_METAS 31
 
 extern uint64_t rdtsc();
 extern void baseline(unsigned n, float** a, float** b, float** c);
@@ -20,7 +20,6 @@ int main(int argc, char *argv[]){
 	unsigned int i,j,n,m;
 	int repm,repw;
 	uint64_t t1,t2;
-	clock_t start,end;
 
 	n = atoi(argv[1]);
 	repw = atoi(argv[2]);
@@ -60,15 +59,12 @@ int main(int argc, char *argv[]){
 		}
 
 		t1 = rdtsc();
-		start = clock();
 		for(i=0;i<repm;i++){
 			baseline(n,a,b,c);
 		}
-		end = clock();
 		t2 = rdtsc();
 
-		//printf("%.2f cycles/operation\n",(float)(t2 - t1) / ((float) n * n * repm));
-		printf("%.6f\n",(end - start) / ((float) CLOCKS_PER_SEC * repm));
+		printf("%.2f\n",(float)(t2 - t1) / ((float) n * n * repm));
 
 		/*printMat("A",a,n);
 		printMat("B",b,n);
