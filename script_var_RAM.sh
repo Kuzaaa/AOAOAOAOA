3edbb6aebeaaa07fe1165ec84ed0b5d08d7006a3
Live session user <amau.b@hotmail.fr>
2020-04-26 16:54:58 +0000

    Correction script RAM

--- script_var_RAM.sh
+++ script_var_RAM.sh
@@ -5,128 +5,49 @@ rm -r resultsRAM
 mkdir resultsRAM
 
 cc=gcc
-echo "CC : " $cc
+echo "CC : $cc"
 
 mkdir resultsRAM/$cc
 
-for opt in O1 O2 O3 Ofast
+for opt in O2 O3 "O3 -funroll-loops" "O3 -march=native" "O3 -fprofile-use"
 do
-	echo "Opt : " $opt
+	echo "Opt : $opt"
 
-	mkdir resultsRAM/$cc/$opt
+	mkdir "resultsRAM/$cc/$opt"
 
-	for var in baseline tab_1d independant independant_split block_cache block_cache_mod independant_parallel \
-	 	independant_split_parallel block_cache_parallel
+	for var in baseline tab_1d independant independant_split block_cache independant_parallel independant_split_parallel block_cache_parallel
 	do
-		echo "Variante : " $var
-		make $var CC=$cc FLAG=$opt
-		./exec 1000 300 600 >> resultsRAM/$cc/$opt/"resultat_"$var".txt"
+		echo "Variante : $var"
+		make $var FLAG="$opt"
+		./exec 1000 300 600 >> "resultsRAM/$cc/$opt/resultat_$var.txt"
 
 		echo
 	done
 
 	echo
 done
-
-for opt in march=native funroll-loops fsplit-loops fgcse-las fallow-store-data-races funswitch-loops floop-nest-optimize
-do
-	echo "Opt : " $opt
-
-	mkdir resultsRAM/$cc/$opt
-
-	for var in baseline tab_1d independant independant_split block_cache block_cache_mod independant_parallel \
-	 	independant_split_parallel block_cache_parallel
-	do
-		echo "Variante : " $var
-		make $var CC=$cc OFLAGS=-O3 FLAG=$opt
-		./exec 1000 300 600 >> resultsRAM/$cc/$opt/"resultat_"$var".txt"
-
-		echo
-	done
-
-	echo
-done
-
-opt=fprofile-use
-echo "Opt : " $opt
-
-mkdir resultsRAM/$cc/$opt
-
-for var in baseline tab_1d independant independant_split block_cache block_cache_mod independant_parallel \
- 	independant_split_parallel block_cache_parallel
-do
-	echo "Variante : " $var
-	make $var CC=$cc OFLAGS=-O3 FLAG=fprofile-generate
-	./exec 1000 300 600 > /dev/null
-	make $var CC=$cc OFLAGS=-O3 FLAG=$opt
-	./exec 1000 300 600 >> resultsRAM/$cc/$opt/"resultat_"$var".txt"
-
-	echo
-done
-
-echo
-
 
 cc=icc
-echo "CC : " $cc
+echo "CC : $cc"
 
 mkdir resultsRAM/$cc
 
-for opt in O1 O2 O3 Ofast
+for opt in O2 O3 "O3 -xHost"
 do
-	echo "Opt : " $opt
+	echo "Opt : $opt"
 
-	mkdir resultsRAM/$cc/$opt
+	mkdir "resultsRAM/$cc/$opt"
 
-	for var in baseline tab_1d independant independant_split block_cache block_cache_mod independant_parallel \
-	 	independant_split_parallel block_cache_parallel
+	for var in baseline tab_1d independant independant_split block_cache independant_parallel independant_split_parallel block_cache_parallel
 	do
-		echo "Variante : " $var
-		make $var CC=$cc FLAG=$opt
-		./exec 1000 300 600 >> resultsRAM/$cc/$opt/"resultat_"$var".txt"
+		echo "Variante : $var"
+		make $var CC=$cc FLAG="$opt"
+		./exec 1000 300 600 >> "resultsRAM/$cc/$opt/resultat_$var.txt"
 
 		echo
 	done
 
 	echo
 done
-
-for opt in xHost finline parallel funroll-loops
-do
-	echo "Opt : " $opt
-
-	mkdir resultsRAM/$cc/$opt
-
-	for var in baseline tab_1d independant independant_split block_cache block_cache_mod independant_parallel \
-	 	independant_split_parallel block_cache_parallel
-	do
-		echo "Variante : " $var
-		make $var CC=$cc OFLAGS=-O3 FLAG=$opt
-		./exec 1000 300 600 >> resultsRAM/$cc/$opt/"resultat_"$var".txt"
-
-		echo
-	done
-
-	echo
-done
-
-opt=prof-use
-echo "Opt : " $opt
-
-mkdir resultsRAM/$cc/$opt
-
-for var in baseline tab_1d independant independant_split block_cache block_cache_mod independant_parallel \
- 	independant_split_parallel block_cache_parallel
-do
-	echo "Variante : " $var
-	make $var CC=$cc OFLAGS=-O3 FLAG=prof-gen=threadsafe
-	./exec 1000 300 600 > /dev/null
-	make $var CC=$cc OFLAGS=-O3 FLAG=$opt
-	./exec 1000 300 600 >> resultsRAM/$cc/$opt/"resultat_"$var".txt"
-
-	echo
-done
-
-echo
 
 make clean
